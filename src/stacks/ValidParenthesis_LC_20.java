9de2ae00223bc777a865622b6bc84a8b8e7f7be5
raginiyampalli <yraginireddy@gmail.com>
2022-08-01 17:38:17 +0530

    LC 20 - Valid Parenthesis

---
+++ src/stacks/ValidParenthesis_LC_20.java
@@ -0,0 +1,64 @@
+package stacks;
+
+import java.util.Stack;
+
+/**
+ *
+ Maintain list of opening brackets using a stack.
+ For every closing bracket, make sure that the last bracket that was opened matches [ last bracket that was opened = top of the stack].
+ */
+
+public class ValidParenthesis_LC_20 {
+    public static void main(String args[]){
+        Solution solution = new Solution();
+        System.out.println("Validity of {{}}()()(([()])) " + solution.isValid("{{}}()()(([()]))"));
+        System.out.println("Validity of {{}}()()(([()]))) " + solution.isValid("{{}}()()(([()])))"));
+    }
+}
+
+class Solution {
+    /**
+     For every closing bracket, make sure that the last bracket that was opened matches.
+     Maintain list of opening brackets using a stack.
+     */
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        char[] input = s.toCharArray();
+
+        for(char bracket: input){
+            if(isOpenBracket(bracket)){
+                stack.push(bracket);
+            } else {
+                if(stack.empty()) return false;
+                char open = stack.peek();
+                if(match(open,bracket)){
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return stack.empty();
+    }
+
+    public boolean isOpenBracket(char c){
+        return c=='{' || c=='(' || c=='[' ;
+    }
+
+    public boolean match(char open, char close){
+        if(open == '{'){
+            return close == '}';
+        }
+
+        if(open == '['){
+            return close == ']';
+        }
+
+        if(open == '('){
+            return close == ')';
+        }
+
+        return false;
+    }
+}
